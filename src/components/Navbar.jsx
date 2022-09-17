cdb519d9da1c9ff15aae53ab1bbf2da8a8ae7294
Josephkagimu1 <kagimujoseph00@gmail.com>
2022-09-18 01:08:07 +0300

    update for mobile use

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,11 @@ import './Navbar.css';
 import Toggle from './Toggle/Toggle';
 
 import { Link } from 'react-scroll';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+
 
   function Refresh() {
     window.location.reload(false);
@@ -20,29 +23,46 @@ const Navbar = () => {
         </div>
         <div className="n-right">
         <div className="n-list">
+        
+        {show ? (
+            <button 
+            className='nav-btn'
+            onClick={() => setShow(false)}
+            > close </button>
+        ) : (
+          <button 
+            className='nav-btn'
+            onClick={() => setShow(true)}
+            > Nav </button>
+        )}
+        
+
+          {show ? (
             <ul style={{listStyleType: 'none'}}>
 
-                <Link spy={true} to='Navbar' smooth={true} activeClass='activeClass'>
-                  <li>Home</li>
-                </Link>
-                
-                <Link spy={true} to='Services' smooth={true} >
-                  <li>Services</li>
-                </Link>
-                
-                <Link spy={true} to='Experience' smooth={true} >
-                  <li>Experience</li>
-                </Link>
+            <Link spy={true} to='Navbar' smooth={true} activeClass='activeClass'>
+              <li>Home</li>
+            </Link>
+            
+            <Link spy={true} to='Services' smooth={true} >
+              <li>Services</li>
+            </Link>
+            
+            <Link spy={true} to='Experience' smooth={true} >
+              <li>Experience</li>
+            </Link>
 
-                <Link spy={true} to='Portfolio' smooth={true} >
-                  <li>Portifolio</li>
-                </Link>
+            <Link spy={true} to='Portfolio' smooth={true} >
+              <li>Portifolio</li>
+            </Link>
 
-                <Link spy={true} to='Testimonials' smooth={true} >
-                  <li>Testimonials</li>
-                </Link>
-                
-            </ul>
+            <Link spy={true} to='Testimonials' smooth={true} >
+              <li>Testimonials</li>
+            </Link>
+            
+        </ul>
+          ) : null }
+            
         </div>
 
         <Link spy={true} to='Contact-form' smooth={true} >
